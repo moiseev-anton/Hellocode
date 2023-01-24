58e9f0d1480bf4b552e7a3c122fb917faa966e4b
anton moiseev <moucee13@gmail.com>
2023-01-24 23:14:15 +0300

    Добавили IfElse project

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1,10); // Рандом 1 2 3 . . . 9
+int numberB = new Random().Next(1,10);
+Console.Write("A = ");
+Console.WriteLine(numberA);
+Console.Write("B = ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("A + B = ");
 Console.WriteLine(result);
